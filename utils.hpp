fb7fe732a9b8e79d506bb5cbadfe513e7c52779f
Pranjal <pranjalkhatri950@gmail.com>
2023-10-14 15:24:33 +0530

    Adding file ops

--- utils.hpp
+++ utils.hpp
@@ -3,7 +3,8 @@
 
 #include<stdlib.h>
 #include<time.h>
-
+#include<fstream>
+#include <iostream>
 class Random
 {
 private:
@@ -12,6 +13,29 @@ public:
     int GetRandomNumber(int lb,int ub);
     int GetRandomNumber(int range);
 };
+
+class FileOps
+{
+private:
+    std::string f_name;
+public:
+    FileOps(std::string name);
+    ~FileOps() = default;
+    void Write(std::string message);
+};
+
+FileOps::FileOps(std::string name)
+{
+    f_name = name;
+}
+
+void FileOps::Write(std::string message)
+{
+    std::fstream f(f_name,std::ios_base::app | std::ios_base::in);
+    f<<message;
+    f.close();
+}
+
 
 
 
